7db5635c0e4d5a421748a1a152b7bf7242c7d31e
Samuel260107 <130668952+Samuel260107@users.noreply.github.com>
2023-08-17 11:31:43 -0300

    samuel

--- main.js
+++ main.js
@@ -2,9 +2,9 @@ let numero = Number(prompt("Informe o número: "));
 let resultado;
 
 
-resultado = numero&2;
+resultado = numero%2;
 
-if(resultado = 1){
+if(resultado == 1){
 document.write("O número " + numero + " é: ÍMPAR");
 } else{
 document.write("O número " + numero + " é: PAR")
